7907b6d7b0bfd67d194375e67b71c2a45b48f8ad
Kleiton Utida <kleiton.utida@live.com>
2023-01-07 02:26:48 -0300

    Comments

---
+++ src/main/kotlin/fundamentos/Comentarios.kt
@@ -0,0 +1,24 @@
+package fundamentos
+
+/**
+ * Só para dizer que o kotlin suporta o KDoc... :)
+ *
+ * @param args lista de parâmetros passados por linha de comando
+ */
+fun main(args: Array<String>) {
+    // Comentário de uma linha
+    println("Opa")
+
+    /*
+      Mais de
+      uma linha
+     */
+    println("Sério?")
+
+    /*
+     * Mais de
+     * uma linha
+     * com estilo
+     */
+    println("Legal!")
+}
