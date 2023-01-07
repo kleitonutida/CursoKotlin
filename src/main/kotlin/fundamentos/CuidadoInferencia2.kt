3f32f75360c655d8229ec81add7b88799c89cc85
Kleiton Utida <kleiton.utida@live.com>
2023-01-07 02:36:38 -0300

    Careful inference 2

---
+++ src/main/kotlin/fundamentos/CuidadoInferencia2.kt
@@ -0,0 +1,10 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    var a: Double = 1.0
+    var b = 2
+
+    a = 2.3 // Para atribuir o valor a uma variável declarada ela precisa ser do mesmo tipo
+
+    print(a + b)
+}
