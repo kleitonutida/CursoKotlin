f3da258b1a3fd33301be529334fd6ec101114460
Kleiton Utida <kleiton.utida@live.com>
2023-01-18 23:32:37 -0300

    Standard Params

--- src/main/kotlin/funcoes/ParametrosImutaveis.kt
+++ src/main/kotlin/funcoes/ParametrosImutaveis.kt
@@ -2,7 +2,7 @@ package funcoes
 
 // Todos os parâmetros de uma função são imutáveis
 fun incremento(num: Int) {
-    num++ // num = num + 1
+//    num++ // num = num + 1
 }
 
 fun main(args: Array<String>) {
