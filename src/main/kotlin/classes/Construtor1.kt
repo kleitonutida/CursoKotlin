474c80a8eb52d940c3cb217d08d7c5c84646ffd6
Kleiton Utida <kleiton.utida@live.com>
2023-02-05 23:29:22 -0300

    Constructor

---
+++ src/main/kotlin/classes/Construtor1.kt
@@ -0,0 +1,24 @@
+package classes
+
+class Filme {
+    val nome: String
+    val anoLancamento: Int
+    val genero: String
+
+    /*
+     * O construtor em Kotlin não segue o padrão de nomenclatura de mesmo nome da classe
+     * Para se criar um construtor é necessário incluir a palavra "constructor"
+     * O construtor é o responsável pela criação do objeto informando as variáveis ou constantes
+     * necessárias para se criar uma instância de um objeto
+     */
+    constructor(nome: String, anoLancamento: Int, genero: String) {
+        this.nome = nome
+        this.anoLancamento = anoLancamento
+        this.genero = genero
+    }
+}
+
+fun main(args: Array<String>) {
+    val filme = Filme(nome = "O Poderoso Chefão", anoLancamento = 1972, genero = "Drama")
+    println("O ${filme.genero} ${filme.nome} foi lançado em ${filme.anoLancamento}")
+}
