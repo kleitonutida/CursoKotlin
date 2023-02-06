d9a782d234311453ee72cc8c1474387c1953412e
Kleiton Utida <kleiton.utida@live.com>
2023-02-05 23:35:46 -0300

    Constructor

---
+++ src/main/kotlin/classes/Construtor2.kt
@@ -0,0 +1,13 @@
+package classes
+
+/*
+ * Os parâmetros informados após o nome da classe, serão utilizados para determinar o construtor da classe
+ * Neste construtor é possível utilizar os parâmetros nomeados e também informar um valor padrão para a
+ * variável
+ */
+class Filme2(val nome:String, val anoLancamento:Int, val genero:String)
+
+fun main(args: Array<String>) {
+    val filme = Filme2(nome = "Monstros S.A", anoLancamento = 2001, genero = "Animação")
+    println("A ${filme.genero} ${filme.nome} foi lançado em ${filme.anoLancamento}.")
+}
