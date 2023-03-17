ebc0bcf4160d86db8a9b2dc1ade000f93fc6fdab
Kleiton Utida <kleiton.utida@live.com>
2023-03-17 01:45:59 -0300

    Comentários

--- src/main/kotlin/fundamentos/Comentarios.kt
+++ src/main/kotlin/fundamentos/Comentarios.kt
@@ -3,6 +3,10 @@ package fundamentos
 /**
  * Só para dizer que o kotlin suporta o KDoc... :)
  *
+ * Existe uma ferramenta chamada Dokka utilizada para interpretar as documentações feitas em KDoc e
+ * gerar e exportar a documentação.
+ * https://github.com/Kotlin/dokka
+ *
  * @param args lista de parâmetros passados por linha de comando
  */
 fun main(args: Array<String>) {
