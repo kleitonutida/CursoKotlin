78c189e8ed2e30a3c72fd4c692787c5e2dcf7b03
Kleiton Utida <kleiton.utida@live.com>
2023-03-17 01:53:52 -0300

    Variáveis

--- src/main/kotlin/fundamentos/Var.kt
+++ src/main/kotlin/fundamentos/Var.kt
@@ -1,8 +1,16 @@
 package fundamentos
 
+/**
+ * O Kotlin é uma linguagem fortemente tipada, ou seja, ao iniciar ela possui um tipo definido
+ */
 fun main(args: Array<String>) {
-    var a: Int
-    var b = 2 // Tipo inferido
+    var a: Int // Variável 'a' do tipo inteiro sem atribuição de valor
+
+    /**
+     * Tipo inferido, ou seja, apesar de não estar explícito o tipo da variável,
+     * o Kotlin faz a inferência do tipo a partir do valor atribuído
+     */
+    var b = 2
 
     a = 10
 
