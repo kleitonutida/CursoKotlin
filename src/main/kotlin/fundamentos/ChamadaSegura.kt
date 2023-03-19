388134740884e72d61b6fec39aef539d0310b59b
Kleiton Utida <kleiton.utida@live.com>
2023-03-19 00:55:42 -0300

    Safe call operator

--- src/main/kotlin/fundamentos/ChamadaSegura.kt
+++ src/main/kotlin/fundamentos/ChamadaSegura.kt
@@ -1,6 +1,11 @@
 package fundamentos
 
 fun main(args: Array<String>) {
+    /**
+     * O operador de chamada segura ou safe call operator é o '?'
+     * Com isso a variável pode ter um valor opcional, ou seja, ela não precisa ter valores ou
+     * simplesmente receber nulo
+     */
     var a: Int? = null // Safe call operator
     println(a?.dec())
 }
