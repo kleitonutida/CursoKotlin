e1e4c0fb590bfed88412ea3675551e544a9c0570
Kleiton Utida <kleiton.utida@live.com>
2023-03-19 03:20:55 -0300

    Imports

--- src/main/kotlin/fundamentos/TesteImport.kt
+++ src/main/kotlin/fundamentos/TesteImport.kt
@@ -1,11 +1,18 @@
 package fundamentos
 
+/**
+ * Pode ser utilizado um alias com a palavra 'as' para determinar um outro nome para a função
+ */
 import fundamentos.pacoteA.simplesFuncao as funcaoSimples
 import fundamentos.pacoteA.Coisa
 import fundamentos.pacoteA.FaceMoeda.CARA
 import fundamentos.pacoteB.*
 
 fun main(args: Array<String>) {
+    /**
+     * Importando uma função de um pacote padrão do Kotlin
+     * Não há a necessiade de deixar explícito ou de realizar o import para estes pacotes
+     */
     kotlin.io.println(funcaoSimples("OK"))
 
     val coisa = Coisa("Bola")
