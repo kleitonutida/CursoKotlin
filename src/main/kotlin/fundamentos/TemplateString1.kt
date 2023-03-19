b794c3f3c1c382b655cfd524439e02a448adfb75
Kleiton Utida <kleiton.utida@live.com>
2023-03-19 00:29:51 -0300

    String template

--- src/main/kotlin/fundamentos/TemplateString1.kt
+++ src/main/kotlin/fundamentos/TemplateString1.kt
@@ -2,5 +2,13 @@ package fundamentos
 
 fun main(args: Array<String>) {
     val aprovados = listOf("João", "Maria", "Pedro")
+    /**
+     * Impressão de um texto com a substituição do valor que está dentro de '${}'
+     */
     print("O primeiro colocado foi ${aprovados[0]}.")
+
+    /**
+     * Ao invés de utilizar o template string, pode ser utilizado a concatenação strings.
+     */
+    print("O primeiro colocado foi" + aprovados[0] + ".")
 }
--- src/main/kotlin/fundamentos/TemplateString2.kt
+++ src/main/kotlin/fundamentos/TemplateString2.kt
@@ -2,5 +2,10 @@ package fundamentos
 
 fun main(args: Array<String>) {
     val bomHumor = true
+    /**
+     * String template sendo utilizado para interpretação de uma expressão dentro de um texto
+     * Neste caso o próprio Kotlin consegue interpretar corretamente o que está dentro de '${}' não
+     * sendo necessário incluir o caractere de escape para tratar as aspas duplas
+     */
     print("Hoje estou ${if (bomHumor) "feliz" else "chateado"}.")
 }
