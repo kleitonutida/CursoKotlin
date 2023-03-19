333b4fe85fa4cde0dd22cce6efa8a69f055c4c20
Kleiton Utida <kleiton.utida@live.com>
2023-03-19 02:57:54 -0300

    Forçando NPE

--- src/main/kotlin/fundamentos/ForcandoExcecaoNullPointer.kt
+++ src/main/kotlin/fundamentos/ForcandoExcecaoNullPointer.kt
@@ -2,8 +2,14 @@ package fundamentos
 
 fun main(args: Array<String>) {
     var a: Int? = null
-    println(a?.inc()) // Utilizando o operador null safety para chamada de uma função
+    /**
+     * Utilizando o operador null safety para chamada de uma função
+     */
+    println(a?.inc())
 
     println("Momento do erro...")
-    println(a!!.inc()) // Forçando a exibição de NullPointer ao chamar uma função em uma propriedade nula
+    /**
+     * Forçando a exibição de NullPointer ao chamar uma função em uma propriedade nula
+     */
+    println(a!!.inc())
 }
