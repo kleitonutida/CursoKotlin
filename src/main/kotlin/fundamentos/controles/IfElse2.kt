46a49081f8bac8d1704573a787ca9940d0dff975
Kleiton Utida <kleiton.utida@live.com>
2023-03-20 23:53:52 -0300

    If como expressão

--- src/main/kotlin/fundamentos/controles/IfElse2.kt
+++ src/main/kotlin/fundamentos/controles/IfElse2.kt
@@ -4,6 +4,10 @@ fun main(args: Array<String>) {
     val num1: Int = 7
     val num2: Int = 3
 
+    /**
+     * O if pode ser utilizado como expressão, mas o retorno será a última sentença de código dentro do bloco
+     * Dentro do bloco também é possível colocar uma lógica, respeitando o retorno da sentença
+     */
     val maiorValor = if (num1 > num2) {
         println("processando if...")
         num1
@@ -12,7 +16,10 @@ fun main(args: Array<String>) {
         num2
     }
 
-    // Forma resumida
+    /**
+     * Forma resumida de se utilizar o if como expressão.
+     * Neste caso, não é possível ter um bloco de código para incluir uma lógica
+     */
 //    val maiorValor = if (num1 > num2) num1 else num2
 
     println("O maior valor é $maiorValor.")
