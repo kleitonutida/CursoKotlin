79459bc06cb70cf3038e83ecfc8605dc6cc5701b
Kleiton Utida <kleiton.utida@live.com>
2023-03-20 23:49:39 -0300

    If-else

--- src/main/kotlin/fundamentos/controles/If.kt
+++ src/main/kotlin/fundamentos/controles/If.kt
@@ -3,6 +3,9 @@ package fundamentos.controles
 fun main(args: Array<String>) {
     val nota: Double = 8.3
 
+    /**
+     * Condição para exibição da mensagem
+     */
     if (nota >= 7.0) {
         println("Aprovado")
     }
--- src/main/kotlin/fundamentos/controles/IfElse.kt
+++ src/main/kotlin/fundamentos/controles/IfElse.kt
@@ -3,6 +3,12 @@ package fundamentos.controles
 fun main(args: Array<String>) {
     val nota: Double = 6.3
 
+    /**
+     * Condição para exibir a mensagem, caso contrário será exibido a outra mensagem
+     *
+     * Dentro de um bloco if-else, pode-se ter várias condições utilizando os operadores lógicos
+     * contanto qque o resultado seja verdadeiro ou falso
+     */
     if (nota >= 7.0) {
         println("Aprovado!!")
     } else {
