f78ca3f7d9e78631b6560d5b09b5e005c6c06380
Kleiton Utida <kleiton.utida@live.com>
2023-03-22 01:18:42 -0300

    Estrutura de controle For

--- src/main/kotlin/fundamentos/controles/For1.kt
+++ src/main/kotlin/fundamentos/controles/For1.kt
@@ -1,7 +1,15 @@
 package fundamentos.controles
 
 fun main(args: Array<String>) {
-    for (i in 1..10){
+    /**
+     * Estrutura de repetição for, utilizando o operador range para uma determinada quantidade
+     * de repetições
+     */
+    for (i in 1..10) {
+        println(i)
+    }
+
+    for (i in 1 until 10) {
         println(i)
     }
 }
--- src/main/kotlin/fundamentos/controles/For3.kt
+++ src/main/kotlin/fundamentos/controles/For3.kt
@@ -1,10 +1,16 @@
 package fundamentos.controles
 
 fun main(args: Array<String>) {
+    /**
+     * Execução de um número determinado de repetições de forma crescente e de 5 em 5
+     */
     for (i in 0..100 step 5) {
         println(i)
     }
 
+    /**
+     * Execução de um número determinado de repetições de forma decrescente e de 5 em 5
+     */
     for (i in 100 downTo 0 step 5) {
         println(i)
     }
