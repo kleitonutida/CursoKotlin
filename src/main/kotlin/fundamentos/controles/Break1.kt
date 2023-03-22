639b4d3fd20b19f0133c682fb5110c3b0d769b57
Kleiton Utida <kleiton.utida@live.com>
2023-03-22 01:45:55 -0300

    Break

--- src/main/kotlin/fundamentos/controles/Break1.kt
+++ src/main/kotlin/fundamentos/controles/Break1.kt
@@ -2,6 +2,10 @@ package fundamentos.controles
 
 fun main(args: Array<String>) {
     for (i in 1..10) {
+        /**
+         * Condição para executar o break
+         * O break ele vai incidir sobre o bloco de repetição mais próximo associado a ele
+         */
         if (i == 5) {
             break
         }
--- src/main/kotlin/fundamentos/controles/Break2.kt
+++ src/main/kotlin/fundamentos/controles/Break2.kt
@@ -3,6 +3,10 @@ package fundamentos.controles
 fun main(args: Array<String>) {
     externo@ for (i in 1..15) {
         for (j in 1..15) {
+            /**
+             * O break rotulado pode ser utilizado para não incidir sobre o laço de repetição mais
+             * próximo e sim sobre outro laço de repetição que esteja com o rótulo
+             */
             if (i == 2 && j == 9) break@externo
             println("$i $j")
         }
