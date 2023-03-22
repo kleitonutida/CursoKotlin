43daf5e637be11f1796426e85bdae58be6042d8c
Kleiton Utida <kleiton.utida@live.com>
2023-03-22 00:25:49 -0300

    Estrutura de controle While

--- src/main/kotlin/fundamentos/controles/While1.kt
+++ src/main/kotlin/fundamentos/controles/While1.kt
@@ -3,7 +3,14 @@ package fundamentos.controles
 fun main(args: Array<String>) {
     var opcao: Int = 0
 
+    /**
+     * Estrutura de repetição ideal para situações em que não se sabe a quantidade determinada
+     * de repetições
+     */
     while (opcao != -1) {
+        /**
+         * O readline é utilizado para receber um valor digitado no console
+         */
         val line = readLine() ?: "0"
         opcao = line.toIntOrNull() ?: 0
 
--- src/main/kotlin/fundamentos/controles/While2.kt
+++ src/main/kotlin/fundamentos/controles/While2.kt
@@ -3,6 +3,9 @@ package fundamentos.controles
 fun main() {
     var contador: Int = 1
 
+    /**
+     * Controle da quantidade de repetições, neste caso o ideal é utilizar o laço for
+     */
     while (contador <= 10) {
         println(contador)
         contador++
