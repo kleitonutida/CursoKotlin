c1f6ccb599b99b50b8c50ed6c47770b3ae6c100b
Kleiton Utida <kleiton.utida@live.com>
2023-03-22 01:49:34 -0300

    Continue

--- src/main/kotlin/fundamentos/controles/Continue.kt
+++ src/main/kotlin/fundamentos/controles/Continue.kt
@@ -2,6 +2,10 @@ package fundamentos.controles
 
 fun main(args: Array<String>) {
     for (i in 1..10) {
+        /**
+         * O continue faz com que vá para o próximo valor do laço de repetição interrompendo a execução
+         * do valor atual
+         */
         if (i == 5) {
             continue
         }
