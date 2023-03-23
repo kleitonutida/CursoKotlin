b15fc9545630924dab024287302911d2ba2f4e29
Kleiton Utida <kleiton.utida@live.com>
2023-03-23 00:41:34 -0300

    Destructuring operator

--- src/main/kotlin/fundamentos/operadores/Destructuring.kt
+++ src/main/kotlin/fundamentos/operadores/Destructuring.kt
@@ -3,12 +3,22 @@ package fundamentos.operadores
 data class Carro(val marca: String, val modelo: String)
 
 fun main(args: Array<String>) {
+    /**
+     * Utilização do operador destructuring em cima de uma classe
+     */
     val (marca, modelo) = Carro("Ford", "Fusion")
     println("$marca $modelo")
 
+    /**
+     * Utilização do operador destructuring em cima de uma lista com dois elementos
+     */
     val (marido, mulher) = listOf("João", "Maria")
     println("$marido e $mulher")
 
+    /**
+     * Utilização do operador destructuring em cima de uma lista com três elementos em que será
+     * considerado apenas o terceiro elemento
+     */
     val (_, _, terceiroLugar) = listOf("Kimi", "Hamilton", "Alonso")
     println("$terceiroLugar terminou em terceiro lugar.")
 }
