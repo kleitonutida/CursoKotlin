96df29b4119776673bfcf13fdb77825bdb16b6a3
Kleiton Utida <kleiton.utida@live.com>
2023-03-25 23:46:26 -0300

    Conversão de número e string

--- src/main/kotlin/fundamentos/ConversaoNumeroString.kt
+++ src/main/kotlin/fundamentos/ConversaoNumeroString.kt
@@ -3,10 +3,17 @@ package fundamentos
 fun main(args: Array<String>) {
     val a = 1
 
-    // Número para String
+    /**
+     * Número para String
+     * Neste caos como o número foi convertido para String, o resultado será a concatenação dos valores
+     */
     println(a.toString() + 1)
 
-    // String para Número
+    /**
+     * String para Número
+     * Neste caso como o número foi convertido para Double, o resultado será a soma entre os valores
+     * Lembrando que para casas decimais deve-se utilizar o "." ao invés da ","
+     */
     println("1.9".toDouble() + 3)
     println("Teste".toIntOrNull())
     println("Teste".toIntOrNull() ?: 0)
