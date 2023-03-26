23774a5ca9eff4fcfe4562a06a5495f48ae94893
Kleiton Utida <kleiton.utida@live.com>
2023-03-25 23:33:38 -0300

    Operadores de incremento e decremento

--- src/main/kotlin/fundamentos/operadores/Unarios.kt
+++ src/main/kotlin/fundamentos/operadores/Unarios.kt
@@ -4,23 +4,29 @@ fun main(args: Array<String>) {
     var num1: Int = 1
     var num2: Int = 1
 
+    /**
+     * Adiciona 1 ao valor da variável, ou seja, está alterando o valor da variável e neste caso
+     * não poderia ser uma constante.
+     */
     num1++
     println(num1)
+    /**
+     * Diminui 1 ao valor da variável, ou seja, está alterando o valor da variável e neste caso
+     * não poderia ser uma constante.
+     */
     --num1
     println(num1)
 
     // Incremento e decremento
-    /*
-     * PÓS-FIXADOS
-     * Os operadores pré-fixados (++num1) terão precedência em relação à com0paração, ou seja,
+    /**
+     * PRÉ-FIXADO
+     * Os operadores pré-fixados (++num1) terão precedência em relação à comparação, ou seja,
      * será feito a execução do mesmo e depois será feito a comparação
      *
-     * PRÉ-FIXADOS
+     * PÓS-FIXADO
      * Os operadores pós-fixados (num2--) terão uma precedência menor em relação à comparação, ou seja,
      * a comparação será feita antes do decremento da variável
      */
     println(++num1 == num2--)
     println(num1 == num2)
-
-
 }
