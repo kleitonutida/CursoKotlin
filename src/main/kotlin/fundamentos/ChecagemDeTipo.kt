fb95feebf6b608b83bcf06599baae5f7473449a3
Kleiton Utida <kleiton.utida@live.com>
2023-03-25 23:54:28 -0300

    Checagem de tipos

--- src/main/kotlin/fundamentos/ChecagemDeTipo.kt
+++ src/main/kotlin/fundamentos/ChecagemDeTipo.kt
@@ -2,10 +2,14 @@ package fundamentos
 
 fun main(args: Array<String>) {
     val valor = "abc"
+//    val valor: Any = 123 // Expressão para testar um valor que não é uma String
 
+    // Valida se o valor da variável é uma String
     if (valor is String) {
         println(valor)
-    } else if (valor !is String) {
+    }
+    // Valida se o valor da variável não é uma String
+    else if (valor !is String) {
         println("Não é uma String")
     }
 }
