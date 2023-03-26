20d2175cb928ebb847b65e207a1c98273daa1db2
Kleiton Utida <kleiton.utida@live.com>
2023-03-26 00:50:10 -0300

    Operador cast

--- src/main/kotlin/fundamentos/OperadorCast.kt
+++ src/main/kotlin/fundamentos/OperadorCast.kt
@@ -1,6 +1,9 @@
 package fundamentos
 
 fun imprimirConceito(nota: Any) {
+    /**
+     * Realizando o cast de forma explícita e utilizando o "?" para realizar o cast seguro
+     */
     when (nota as? Int) {
         10, 9 -> println("A")
         8, 7 -> println("B")
@@ -14,6 +17,11 @@ fun imprimirConceito(nota: Any) {
 fun main(args: Array<String>) {
     val notas = arrayOf(9.6, 3.8, 7.2, 5.5, 4.1)
     for (nota in notas) {
+        /**
+         * Chamando a função passando os valores como inteiros
+         * Caso fosse chamado sem converter e na linha 7 não estivesse sendo feito o cast seguro,
+         * ocorreria o erro de ClassCastException
+         */
         imprimirConceito(nota.toInt())
     }
 }
