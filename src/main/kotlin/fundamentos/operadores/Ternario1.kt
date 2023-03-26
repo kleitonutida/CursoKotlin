47d8da83990a2df0d7143cbc7f87bfb3fb5fc97f
Kleiton Utida <kleiton.utida@live.com>
2023-03-25 23:40:45 -0300

    Simulando Ternário

--- src/main/kotlin/fundamentos/operadores/Ternario1.kt
+++ src/main/kotlin/fundamentos/operadores/Ternario1.kt
@@ -2,6 +2,10 @@ package fundamentos.operadores
 
 fun main(args: Array<String>) {
     val nota: Double = 6.2
+
+    /**
+     * Utilizando o if-expression como um operador ternário.
+     */
     val resultado: String = if (nota >= 7) "Aprovado" else "Reprovado"
     println(resultado)
 }
--- src/main/kotlin/fundamentos/operadores/Ternario2.kt
+++ src/main/kotlin/fundamentos/operadores/Ternario2.kt
@@ -1,5 +1,8 @@
 package fundamentos.operadores
 
+/**
+ * Criação de uma função que receber um valor como parâmetro e retorna o resultado da expressão if
+ */
 fun obterResultado(nota: Double): String = if (nota >= 7.0) "Passou" else "Reprovou"
 
 fun main(args: Array<String>) {
