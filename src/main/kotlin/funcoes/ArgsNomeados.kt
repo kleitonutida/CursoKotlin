0148fab750a45e74be0c75d0cb485890db233361
Kleiton Utida <kleiton.utida@live.com>
2023-03-26 23:39:50 -0300

    Argumentos nomeados

--- src/main/kotlin/funcoes/ArgsNomeados.kt
+++ src/main/kotlin/funcoes/ArgsNomeados.kt
@@ -1,8 +1,20 @@
 package funcoes
 
+/**
+ * Criando uma função que recebe dois parâmetros do tipo string e que retorna um texto específico
+ */
 fun relacaoDeTrabalho(chefe: String, funcionario: String): String = "$funcionario é subordinado(a) à $chefe"
 
 fun main(args: Array<String>) {
+    /**
+     * Chamando a função sem passar os argumentos nomeados
+     * Note que sem a ajuda da IDE não é possível identificar quem seria o funcionário e quem seria
+     * o chefe apenas olhando para os parâmetros da função
+     */
     println(relacaoDeTrabalho("João", "Maria"))
+    /**
+     * Chamando a função passando os argumentos nomeados
+     * Note que neste caso acaba ficando mais claro quem é o funcionário e que é o chefe
+     */
     println(relacaoDeTrabalho(funcionario = "João", chefe = "Maria"))
 }
