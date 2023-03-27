4fbe4b18611724ef3204c2b7ed0a13698dba6eb0
Kleiton Utida <kleiton.utida@live.com>
2023-03-27 01:51:15 -0300

    Extension function

--- src/main/kotlin/funcoes/SegundoElementoList.kt
+++ src/main/kotlin/funcoes/SegundoElementoList.kt
@@ -1,8 +1,15 @@
 package funcoes
 
+/**
+ * Criando uma função de extensão para retornar o segundo elemento de uma lista, caso contrário
+ * será retornado nulo
+ */
 fun <E> List<E>.secondOrNull(): E? = if (this.size >= 2) this.get(1) else null
 
 fun main(args: Array<String>) {
     val list = listOf("João", "Maria", "Pedro")
     println(list.secondOrNull())
+
+    val listWithoutSecondElement = listOf("João")
+    println(listWithoutSecondElement.secondOrNull())
 }
