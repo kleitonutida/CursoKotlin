37b4a3383ac9588247c6458b9aa58ebc69936d56
Kleiton Utida <kleiton.utida@live.com>
2023-03-26 23:16:14 -0300

    Funções top-level

--- src/main/kotlin/funcoes/TopLevel.kt
+++ src/main/kotlin/funcoes/TopLevel.kt
@@ -1,6 +1,16 @@
 package funcoes
 
+/**
+ * Função top-level para retornar o menor valor entre duas variáveis
+ */
 fun min(a: Int, b: Int): Int = if (a < b) a else b
+
+/**
+ * A Função também pode ser declarada com um corpo e retornando o valor nele
+ */
+// fun min(a: Int, b: Int): Int {
+//    return if (a < b) a else b
+// }
 
 fun main(args: Array<String>) {
     print("O menor valor é ${min(3, 4)}")
