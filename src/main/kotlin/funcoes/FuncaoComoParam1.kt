ff4da302a68317b5e00fadf8de08fa230c7b890d
Kleiton Utida <kleiton.utida@live.com>
2023-03-29 02:33:09 -0300

    Função como parâmetro

--- src/main/kotlin/funcoes/FuncaoComoParam1.kt
+++ src/main/kotlin/funcoes/FuncaoComoParam1.kt
@@ -10,11 +10,31 @@ fun somar(a: Int, b: Int): Int {
     return a + b
 }
 
+/**
+ * Função que receber três argumentos
+ * Os argumentos "a" e "b" são do tipo Int
+ * A "funcao" é uma função que espera receber dois argumentos do tipo Int e retornar um valor Int,
+ * ou seja, o parâmetro recebido deve ser uma função que tenha dois argumentos do tipo Int e um retorno
+ * do tipo Int
+ *
+ * Esta função é um função de ordem superior que recebe como parâmetro um função que será executada com
+ * os valores de "a" e "b" e retorna um resultado. Isso permite que o comportamento da função possa ser
+ * alterado dinamicamente, dependendo da função passada como parâmetro.
+ */
 fun calc(a: Int, b: Int, funcao: (Int, Int) -> Int): Int {
     return funcao(a, b)
 }
 
 fun main(args: Array<String>) {
+    /**
+     * Chamando a função "calc" passando a referência da função somar da classe
+     * Operacoes através do operador "::". Para isso é necessário instanciar a classe
+     */
     println(calc(2, 3, Operacoes()::somar))
+    /**
+     * Chamando a função "calc" passando a referência da função top-level somar através do operador "::"
+     * A função somar está sem os parênteses "()" porque está sendo passado a referência da função, quem
+     * está invocando a função somar é a função "calc" que recebe a função como parâmetro
+     */
     println(calc(2, 3, ::somar))
 }
--- src/main/kotlin/funcoes/FuncaoComoParam2.kt
+++ src/main/kotlin/funcoes/FuncaoComoParam2.kt
@@ -1,5 +1,9 @@
 package funcoes
 
+/**
+ * Função genérica que realiza o filtro de acordo com a função recebida como parâmetro
+ * Este filtro deverá receber um valor genérico e retornar um valor booleano
+ */
 fun <E> filtrar(lista: List<E>, filtro: (E) -> Boolean): List<E> {
     val listaFiltrada = ArrayList<E>()
     for (e in lista) {
@@ -10,14 +14,23 @@ fun <E> filtrar(lista: List<E>, filtro: (E) -> Boolean): List<E> {
     return listaFiltrada
 }
 
+/**
+ * Função que filtra as palavras que tem três letras
+ */
 fun comTresLetras(nome: String): Boolean {
     return nome.length == 3
 }
 
+/**
+ * Função que filtra as palavras que tem cinco letras
+ */
 fun comCincoLetras(nome: String): Boolean {
     return nome.length == 5
 }
 
+/**
+ * Função que filtra os números maiores que vinte
+ */
 fun maioresQueVinte(numero: Int): Boolean = numero > 20
 
 fun main(args: Array<String>) {
