a9a6508bf23c2c149ed104cb0a6944759364929a
Kleiton Utida <kleiton.utida@live.com>
2023-03-29 23:55:58 -0300

    Função inline

--- src/main/kotlin/funcoes/Inline1.kt
+++ src/main/kotlin/funcoes/Inline1.kt
@@ -1,6 +1,21 @@
 package funcoes
 
+/**
+ * Funções inline são funções que são compiladas diretamente no local que é realizado a chamada dela.
+ * Com isso, pode melhorar a eficiência do código em tempo de execução com a redução do número de chamadas
+ * de função e principalmente no reaproveitamento de um determinado trecho de código que não precisa ser
+ * repetido várias vezes
+ */
 inline fun transacao(funcao: () -> Unit) {
+    println("Abrindo transação...")
+    try {
+        funcao()
+    } finally {
+        println("Fechando transação")
+    }
+}
+
+inline fun transacao1(a: Int, funcao: () -> Unit) {
     println("Abrindo transação...")
     try {
         funcao()
@@ -15,4 +30,33 @@ fun main(args: Array<String>) {
         println("Executantdo SQL 2...")
         println("Executantdo SQL 3...")
     }
+
+    /**
+     * Chamando a função, passando o conjunto de funções como parâmetro diretamente entre os
+     * parênteses
+     */
+    transacao({
+        println("Executantdo SQL 1...")
+        println("Executantdo SQL 2...")
+        println("Executantdo SQL 3...")
+    })
+
+    /**
+     * Chamando a função com mais de um parâmetro, onde a função é passada fora dos parênteses
+     * Obs.: Desta forma só irá funcionar se a função for o último parâmetro
+     */
+    transacao1(a = 1) {
+        println("Executantdo SQL 1...")
+        println("Executantdo SQL 2...")
+        println("Executantdo SQL 3...")
+    }
+
+    /**
+     * Chamando a função com mais de um parâmetro, passando a função dentro dos parênteses
+     */
+    transacao1(a = 1, {
+        println("Executantdo SQL 1...")
+        println("Executantdo SQL 2...")
+        println("Executantdo SQL 3...")
+    })
 }
--- src/main/kotlin/funcoes/Inline2.kt
+++ src/main/kotlin/funcoes/Inline2.kt
@@ -1,5 +1,9 @@
 package funcoes
 
+/**
+ * Criação de uma função que irá exibir um log
+ * Os seus parâmetros serão: o nome da função como texto e uma função anônima
+ */
 inline fun <T> executarComLog(nomeFuncao: String, funcao: () -> T): T {
     println("Entrando no método $nomeFuncao...")
     try {
@@ -9,13 +13,30 @@ inline fun <T> executarComLog(nomeFuncao: String, funcao: () -> T): T {
     }
 }
 
+/**
+ * Esta função foi nomeada para somar2, pois já existe uma função de mesmo nome dentro do mesmo pacote
+ * As funções e as funções inline não podem ter o mesmo nome dentro do mesmo pacote
+ */
 fun somar2(a: Int, b: Int): Int {
     return a + b
 }
 
 fun main(args: Array<String>) {
+    /**
+     * Chamando a função executarComLog passando um lambda como função anônima e não diretamente a
+     * função de somar
+     * A última sentença de código da função lambda é o que será retornado, já que a função
+     * executarComLog retorna o mesmo tipo de função passada como parâmetro
+     */
     val resultado = executarComLog("somar") {
         somar2(4, 5)
     }
+
+    /**
+     * A função pode ser chamada também desta forma
+     */
+//    val resultado = executarComLog("somar", {
+//        somar2(4, 5)
+//    })
     println(resultado)
 }
