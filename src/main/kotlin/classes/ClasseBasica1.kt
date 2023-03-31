e6105c4a5d87d77686ec02bcab2f1ae36a6bd789
Kleiton Utida <kleiton.utida@live.com>
2023-03-30 23:41:40 -0300

    Classe básica

--- src/main/kotlin/classes/ClasseBasica1.kt
+++ src/main/kotlin/classes/ClasseBasica1.kt
@@ -1,6 +1,18 @@
 package classes
 
+/**
+ * Para se declarar uma classe, é necessário utilizar a palavra "class" e logo em seguida definir
+ * um nome para a classe
+ *
+ * O construtor de uma classe tem o mesmo nome da classe e por padrão toda a classe tem o seu
+ * construtor padrão que não recebe nenhum parâmetro
+ */
 class Cliente {
+    /**
+     * Dentro de classes temos os membros, que são atributos e comportamentos
+     * Os atributos são os dados
+     * Os comportamentos são as funções / métodos
+     */
     var nome: String = ""
 }
 
--- src/main/kotlin/classes/ClasseBasica2.kt
+++ src/main/kotlin/classes/ClasseBasica2.kt
@@ -1,13 +1,23 @@
 package classes
 
-// Após instanciar a classe, o atributo nome estará visível para leitura e escrita
-class Pessoa1(var nome:String)
+/**
+ * Definindo o contrutor da classe que recebe uma variável como parâmetro
+ * Após instanciar a classe, o atributo nome estará visível para leitura e escrita
+ */
+class Pessoa1(var nome: String)
 
-// Após instanciar a classe, o atributo nome estará visível apenas para leitura
-class Pessoa2(val nome:String)
+/**
+ * Definindo o contrutor da classe que recebe uma constante como parâmetro
+ * Após instanciar a classe, o atributo nome estará visível apenas para leitura
+ */
+class Pessoa2(val nome: String)
 
-// Após instanciar a classe, o atributo nome não estará visível, ele é uma variável local
-// A variável vai estar disponível internamente no construtor ou no processo de inicialização do objeto
+/**
+ * Definindo o construtor da classe que recebe um parâmetro sem definição de variável ou constante
+ * Após instanciar a classe, o atributo nome não estará visível, ele é uma variável local
+ * A variável vai estar disponível internamente no construtor e no processo de inicialização do objeto,
+ * ele não é um atributo de instância que fica disponível quando um objeto é criado
+ */
 class Pessoa3(nomeInicial: String) {
     // Variável criada para disponibilizar o nome
     val nome: String = nomeInicial
