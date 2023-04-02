15f356502f04ab8f0d3187a0fbf150392417283a
Kleiton Utida <kleiton.utida@live.com>
2023-04-01 23:34:29 -0300

    Membros de classe

--- src/main/kotlin/classes/Membros1.kt
+++ src/main/kotlin/classes/Membros1.kt
@@ -1,6 +1,12 @@
 package classes
 
+/**
+ * Classe para representar uma data
+ */
 class Data(var dia: Int, var mes: Int, var ano: Int) {
+    /**
+     * Função para formatar uma data a partir da data informada na classe
+     */
     fun formatar(): String {
         return "$dia/$mes/$ano"
     }
@@ -10,6 +16,10 @@ fun main(args: Array<String>) {
     var nascimento: Data = Data(dia = 11, mes = 10, ano = 2003)
 
     println("${nascimento.dia}/${nascimento.mes}/${nascimento.ano}")
+    /**
+     * Utilizando a função inline "with". Neste caso, não é necessário chamar a instância para
+     * acessar os atributos
+     */
     with(nascimento) { println("$dia/$mes/$ano") }
 
     nascimento.mes = 11
--- src/main/kotlin/classes/Membros2.kt
+++ src/main/kotlin/classes/Membros2.kt
@@ -1,38 +1,68 @@
 package classes
 
+/**
+ * Classe para representar uma calculadora
+ */
 class Calculadora {
+    /**
+     * Variável de instância (objeto) que será criado a cada nova instância da classe Calculadora
+     */
     private var resultado = 0
 
+    /**
+     * Função que realiza a soma de valores na variável resultado e retorna a instância de Calculadora
+     */
     fun somar(vararg valores: Int): Calculadora {
         valores.forEach { resultado += it }
         return this
     }
 
+    /**
+     * Função que realiza a multiplicação do valor na variável resultado
+     * e retorna a instância de Calculadora
+     */
     fun multiplicar(valor: Int): Calculadora {
         resultado *= valor
         return this
     }
 
+    /**
+     * Função que realiza a subtração do valor na variável resultado
+     * e retorna a instância de Calculadora
+     */
     fun subtrair(valor: Int): Calculadora {
         resultado -= valor
         return this
     }
 
+    /**
+     * Função que realiza a divisão do valor na variável resultado
+     * e retorna a instância de Calculadora
+     */
     fun dividir(valor: Int): Calculadora {
         resultado /= valor
         return this
     }
 
+    /**
+     * Função que limpa a variável resultado e retorna a instância de Calculadora
+     */
     fun limpar(): Calculadora {
         resultado = 0
         return this
     }
 
+    /**
+     * Função que imprime o valor da variável resultado e retorna a instância de Calculadora
+     */
     fun print(): Calculadora {
         println(resultado)
         return this
     }
 
+    /**
+     * Função que retorna o valor da variável resultado
+     */
     fun obterResultado(): Int {
         return resultado
     }
@@ -40,6 +70,10 @@ class Calculadora {
 
 fun main(args: Array<String>) {
     val calculadora = Calculadora()
+    /**
+     * Realizando a chamada das funções encadeando umas nas outras
+     * Isso é possível, pois o retorno de algumas das funções é a própria instância de Calculadora
+     */
     calculadora.somar(1, 2, 3).multiplicar(3).print()
     calculadora.somar(7, 10).print().limpar()
 
