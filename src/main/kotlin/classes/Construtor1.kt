5e4785c358ec7e6bd1e5053f7c42d65e7d5357df
Kleiton Utida <kleiton.utida@live.com>
2023-04-02 23:44:23 -0300

    Construtor

--- src/main/kotlin/classes/Construtor1.kt
+++ src/main/kotlin/classes/Construtor1.kt
@@ -1,15 +1,20 @@
 package classes
 
 class Filme {
+    /**
+     * Ao definir constantes, elas precisam ser inicilizadas, caso contrário ocorrerá erro de
+     * compilação do código
+     */
     val nome: String
     val anoLancamento: Int
     val genero: String
 
-    /*
+    /**
      * O construtor em Kotlin não segue o padrão de nomenclatura de mesmo nome da classe
      * Para se criar um construtor é necessário incluir a palavra "constructor"
      * O construtor é o responsável pela criação do objeto informando as variáveis ou constantes
      * necessárias para se criar uma instância de um objeto
+     * Neste caso, pode ser informados também o parâmetro padrão de cada um dos atributos.
      */
     constructor(nome: String, anoLancamento: Int, genero: String) {
         this.nome = nome
--- src/main/kotlin/classes/Construtor2.kt
+++ src/main/kotlin/classes/Construtor2.kt
@@ -1,13 +1,14 @@
 package classes
 
-/*
- * Os parâmetros informados após o nome da classe, serão utilizados para determinar o construtor da classe
- * Neste construtor é possível utilizar os parâmetros nomeados e também informar um valor padrão para a
- * variável
+/**
+ * Os parâmetros informados após o nome da classe, serão utilizados para determinar o construtor da
+ * classe. Neste construtor é possível utilizar os parâmetros nomeados e também informar um
+ * valor padrão para cada uma das variáveis
  */
-class Filme2(val nome:String, val anoLancamento:Int, val genero:String)
+class Filme2(var nome: String, val anoLancamento: Int, val genero: String)
 
 fun main(args: Array<String>) {
     val filme = Filme2(nome = "Monstros S.A", anoLancamento = 2001, genero = "Animação")
-    println("A ${filme.genero} ${filme.nome} foi lançado em ${filme.anoLancamento}.")
+    filme.nome = "Universidade Monstros"
+    println("A ${filme.genero} ${filme.nome} foi lançada em ${filme.anoLancamento}.")
 }
