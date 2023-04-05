f579efb9032a164bd895a32e1c88ea5da91374a9
Kleiton Utida <kleiton.utida@live.com>
2023-04-04 23:36:08 -0300

    Membros de classe vs membros de instância

--- src/main/kotlin/classes/MembrosClasseVsInstancia.kt
+++ src/main/kotlin/classes/MembrosClasseVsInstancia.kt
@@ -1,12 +1,20 @@
 package classes
 
+/**
+ * Função top-level criada para ter um desconto para todas as instâncias da classe
+ *
+ * Obs.: Este tipo de abordagem deve ser evitado para variáveis, pois quanto mais genérico e com
+ * possibilidade de se alterar uma variável de classe, pode ocorrer efeitos colaterais como bugs e
+ * comportamentos indesejados
+ * Neste caso, o ideal é que sejam utilizados constantes ao invés de variáveis
+ */
 var desconto: Double = 0.0
 
 class ItemDePedido(val nome: String, val preco: Double) {
     companion object {
-        fun create(nome:String, preco: Double) = ItemDePedido(nome, preco)
+        fun create(nome: String, preco: Double) = ItemDePedido(nome, preco)
 
-        /*
+        /**
          * Definição do desconto como variável estática de classe, qualquer alteração realizada nela
          * será refletida para todas as instâncias. Neste caso, uma alternativa é a criação de variáveis
          * ou funções top-level
