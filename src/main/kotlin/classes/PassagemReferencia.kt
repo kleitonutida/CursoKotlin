ba818d686e551fad3114410d024a42ffa123d784
Kleiton Utida <kleiton.utida@live.com>
2023-04-04 23:51:28 -0300

    Passagem por referência

--- src/main/kotlin/classes/PassagemReferencia.kt
+++ src/main/kotlin/classes/PassagemReferencia.kt
@@ -1,6 +1,6 @@
 package classes
 
-/*
+/**
  * Os dados são passados por referência e dentro de uma função, as referências são constantes
  *
  * Uma vez que o dado é passado por referência dentro de um método, não pode ser alterado a referência
@@ -9,13 +9,13 @@ package classes
  */
 
 // Erro!! Kotlin: Val cannot be reassigned
-//fun porReferencia(velocidade: Int) {
+// fun porReferencia(velocidade: Int) {
 //    velocidade++
-//}
+// }
 
 data class Carro(var marca: String, var modelo: String, var velocidade: Int = 0)
 
-/*
+/**
  * Neste caso o carro continua com a mesma referência e a velocidade é uma variável
  */
 fun porReferencia(carro: Carro) {
@@ -25,17 +25,17 @@ fun porReferencia(carro: Carro) {
 fun main(args: Array<String>) {
     var carro1 = Carro(marca = "Ford", modelo = "Fusion")
 
-    /*
+    /**
      * Criando "carro2" a partir do "carro1" e alterando o modelo do carro
-     * Esse tipo de atribuição faz com que o "carro2" aponte para a mesma referência de "carro1", sendo
-     * que qualquer alteração dos atribuitos de "carro2" afetará o "carro1" e o inverso também é
-     * verdadeiro
+     * Esse tipo de atribuição faz com que o "carro2" aponte para a mesma referência de "carro1",
+     * sendo que qualquer alteração dos atribuitos de "carro2" afetará o "carro1" e o inverso
+     * também é verdadeiro
      */
     var carro2: Carro = carro1
     carro2.modelo = "Edge"
     println(carro1)
 
-    /*
+    /**
      * Como o "carro1" será atribuido a uma nova instância, os valores de ambos os carros serão
      * diferentes
      */
