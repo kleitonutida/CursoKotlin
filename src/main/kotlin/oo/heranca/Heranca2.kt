12b6b0a4a779dca212df4d1403eb6d1c3aba9949
Kleiton Utida <kleiton.utida@live.com>
2023-04-19 23:53:20 -0300

    Herança

--- src/main/kotlin/oo/heranca/Heranca2.kt
+++ src/main/kotlin/oo/heranca/Heranca2.kt
@@ -5,10 +5,22 @@ open class Animal(val nome: String)
 class Cachorro : Animal {
     private val altura: Double
 
+    /**
+     * Criando um construtor que chama o construtor da classe herdada (pai)
+     * Quando é definido um construtor e a classe herda de uma outra classe, obrigatoriamente o
+     * construtor deverá chamar o construtor da classe herdada utilizando a palavra "super"
+     */
     constructor(nome: String, altura: Double) : super(nome) {
         this.altura = altura
     }
 
+    /**
+     * Criando um construtor que chama o construtor definido dentro da própria classe utilizando
+     * a palavra "this"
+     * Obrigatoriamente a classe herdada deverá ter o seu construtor na definição ou o construtor
+     * pode ser definido em um outro construtor dentro da própria classe, mas o construtor da classe
+     * herdada deverá ser chamado na cadeia
+     */
     constructor(nome: String) : this(nome, 0.0)
 
     override fun toString(): String = "$nome tem $altura cm de altura."
