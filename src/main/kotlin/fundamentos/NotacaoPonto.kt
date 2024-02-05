ce01b74dc86bc15a669838e14833f4ca91bd54f5
Kleiton Utida <kleiton.utida@live.com>
2024-02-05 01:12:27 -0300

    [Fundamentos] Notação ponto

--- src/main/kotlin/fundamentos/NotacaoPonto.kt
+++ src/main/kotlin/fundamentos/NotacaoPonto.kt
@@ -4,13 +4,14 @@ package fundamentos
  * A notação ponto é utilizada para acessar funções membros de um objeto, instância ou classe.
  */
 fun main(args: Array<String>) {
+    // 33.dec() é equivalente a 33 - 1 (notação ponto utilizada para acessar a função dec() de Int)
     val a: Int = 33.dec()
+
+    // a.toString() é equivalente a a.toString(a) (notação ponto utilizada para acessar a função toString() de Int)
     val b: String = a.toString()
 
-    println("Int: $a")
+    println("Int: $a") // Não é necessário utilizar a delimitação de chaves para acessar a variável a
 
-    /**
-     * A notação ponto também pode ser utilizada dentro de template strings
-     */
-    println("Primeiro char da String b é: ${b.first()}")
+    // A notação ponto também pode ser utilizada dentro de template strings
+    println("Primeiro char da String b é: ${b.first()}") // Primeiro char da String b é: 3
 }
