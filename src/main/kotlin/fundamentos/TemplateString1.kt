f0c0c5f80315b5488d274a903b13ad87a2c92f5d
Kleiton Utida <kleiton.utida@live.com>
2024-02-05 00:09:45 -0300

    [Fundamentos] Template String

--- src/main/kotlin/fundamentos/TemplateString1.kt
+++ src/main/kotlin/fundamentos/TemplateString1.kt
@@ -1,14 +1,15 @@
 package fundamentos
 
 fun main(args: Array<String>) {
-    val aprovados = listOf("João", "Maria", "Pedro")
+    val aprovados = listOf("João", "Maria", "Pedro") // Lista com 3 elementos
+
     /**
      * Impressão de um texto com a substituição do valor que está dentro de '${}'
      */
     print("O primeiro colocado foi ${aprovados[0]}.")
 
     /**
-     * Ao invés de utilizar o template string, pode ser utilizado a concatenação strings.
+     * Ao invés de utilizar o template string, pode ser utilizado a concatenação de strings.
      */
     print("O primeiro colocado foi" + aprovados[0] + ".")
 }
--- src/main/kotlin/fundamentos/TemplateString2.kt
+++ src/main/kotlin/fundamentos/TemplateString2.kt
@@ -2,9 +2,10 @@ package fundamentos
 
 fun main(args: Array<String>) {
     val bomHumor = true
+
     /**
-     * String template sendo utilizado para interpretação de uma expressão dentro de um texto
-     * Neste caso o próprio Kotlin consegue interpretar corretamente o que está dentro de '${}' não
+     * String template sendo utilizado para interpretar uma expressão em um texto
+     * Neste caso o próprio Kotlin interpreta o que está entre '${}' não
      * sendo necessário incluir o caractere de escape para tratar as aspas duplas
      */
     print("Hoje estou ${if (bomHumor) "feliz" else "chateado"}.")
